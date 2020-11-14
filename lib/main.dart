d7ba986abac176dde2786c61619fc65b5baec9a3
KaanHGunay <kaangunay16@gmail.com>
2020-11-14 10:56:40 +0300

    The Boss 1 finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,55 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: Home(),
+    ),
+  );
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int ball = 1 + Random().nextInt(5);
+
+  void changeBall() {
+    setState(() {
+      ball = 1 + Random().nextInt(5);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Magic Ball'),
+          backgroundColor: Colors.indigo,
+        ),
+        backgroundColor: Colors.blue,
+        body: Center(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    changeBall();
+                  },
+                  child: Image.asset('images/ball$ball.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
+  }
+}
